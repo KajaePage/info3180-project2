2d59b389cfe23b90027622261cc7ad7ebabb0ce3
Riverine-jpg <nateb2001@gmail.com>
2022-04-25 00:09:29 -0500

    changes

--- src/router/index.js
+++ src/router/index.js
@@ -37,7 +37,7 @@ const router = createRouter({
               {
                 path: '/users/:user_id',
                 name: 'My Page',
-                component: () => import('../views/Userview.vue')
+                component: () => import('../views/UserView.vue')
                 },
                 {
                   path: '/cars/:car_id',
